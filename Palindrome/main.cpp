46fed9278be497e02f7652b416c854a3287442fd
David <davidbrookwell-reuber@David-Brookwell-Reubers-MacBook-Pro.local>
2011-10-27 09:43:29 -0600

    Initial Commit

---
+++ Palindrome/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Palindrome
+//
+//  Created by David Brookwell-Reuber on 11-10-27.
+//  Copyright 2011 Grant MacEwan University. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
